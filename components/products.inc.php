3a6dbeef7d2b55bfe1559c5d68b6a21eb0444fe0
solecardazzo <soledadcardazzo@gmail.com>
2022-08-09 14:45:58 -0300

    final

--- components/products.inc.php
+++ components/products.inc.php
@@ -1,30 +1,64 @@
 <section class="text-center wrapper" id="products">
   <h2>productos</h2>
   <ul class="tabs">
-    <li class="active">PAGINA WEB</li>
+    <li class="active">PLATAFORMA</li>
     <li>BASE DE DATOS</li>
-    <li>PRONOSTICOS DE LLUVIA</li>
+    <!-- <li>PRONOSTICOS DE LLUVIA</li> -->
   </ul>
   <ul class="tab__content">
     <li class="active">
       <div class="content__wrapper">
-        <p class="text-color">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure iste mollitia autem voluptates earum, optio doloremque ipsam voluptatum blanditiis consequatur! Ipsum veniam quas aliquid fugiat omnis nostrum accusantium eligendi esse! </p>
-
-        <ul class="colors">
-        </ul>
+        <p class="text-color col-lg-12 col-md-12 col-sm-12 col-xs-12" >A pesar de los impresionantes avances en  la tecnología las actividades económicas siguen dependiendo del tiempo y el clima.
+          En particular, el daño causado por las heladas a los cultivos ha sido un problema para los humanos desde que comenzaron los primeros cultivos. Incluso si todos los aspectos de la producción agrícola están bien gestionados, una noche de temperaturas bajo cero puede provocar la pérdida parcial o total de la cosecha.
+          En la actualidad las tierras utilizadas para la agricultura ocupan el 11 % de las tierras habitables del planeta donde las heladas perjudican y malogran entre un 5 y 15 % de la producción mundial anual y se gastan millones de dólares en mecanismos para la protección de este fenómeno.
+        </p>
+        <a href="http://www.pronosticosheladas.com/" title="Pronosticos heladas" target="_blank">
+          <img src="img/paginaHeladas.png" class="img-responsive col-lg-7 col-md-7 col-sm-7 col-xs-12 card-1" >
+        </a>
+        <p class="text-color col-lg-5 col-md-5 col-sm-5 col-xs-12" >Es en este sentido es que Aprilis viene a transformar lo hasta ahora conocido en una solución meteorológica brindando un pronóstico de heladas a nivel global utilizando las herramientas más avanzadas de modelización numérica.
+          En nuestra página web se puede consultar dos productos Uno es la intensidad de heladas leves, moderadas o fuertes con diferentes colores para los próximos 9 días y otro es la probabilidad de ocurrencia de intensidad de heladas (también leves, moderadas o fuertes) para los próximos 15 días.
+        </p>
       </div>
     </li>
     <li>
       <div class="content__wrapper">
-        <p class="text-color">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure iste mollitia autem voluptates earum, optio doloremque ipsam voluptatum blanditiis consequatur! Ipsum veniam quas aliquid fugiat omnis nostrum accusantium eligendi esse! </p>
-
+        <section class="features" id="features">
+          <div class="feature-card">
+            <p>
+              <img class="icons" src="https://image.flaticon.com/icons/png/512/25/25674.png" alt="invisible icon" width="50" />
+            </p>
+            <strong>Invisibility</strong>
+            <p>The wearer becomes invisible to physical beings</p>
+          </div>
+          <div class="feature-card">
+            <p>
+              <img class="icons" src="https://image.flaticon.com/icons/png/512/31/31745.png" alt="extended lifespan icon" width="50" />
+            </p>
+            <strong>Extended life span</strong>
+            <p>Extends the lifespan of a mortal bearer indefinitely</p>
+          </div>
+          <div class="feature-card">
+            <p>
+              <img class="icons" src="https://image.flaticon.com/icons/png/512/99/99744.png" alt="Augmented power icon" width="50" />
+            </p>
+            <strong>Augmented power</strong>
+            <p>Enhances the abilities and powers of the wearer</p>
+          </div>
+          <div class="feature-card">
+            <p>
+              <img class="icons" src="https://image.flaticon.com/icons/png/512/93/93640.png" alt="Adaptable size icon" width="50" />
+            </p>
+            <strong>Adaptable size</strong>
+            <p>Has the ability to adapt to fingers of varying size</p>
+          </div>
+        </section>
       </div>
     </li>
-    <li>
+    <!-- <li>
       <div class="content__wrapper">
         <p class="text-color">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure iste mollitia autem voluptates earum, optio doloremque ipsam voluptatum blanditiis consequatur! Ipsum veniam quas aliquid fugiat omnis nostrum accusantium eligendi esse! </p>
 
       </div>
-    </li>
+    </li> -->
   </ul>
 </section>
